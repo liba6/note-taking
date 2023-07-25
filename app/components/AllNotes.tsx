286c185dc93d1f9bd3b0108035c75b6aac6baf29
liba6 <lginsburg@gmail.com>
2023-07-25 22:49:37 +0200

    Edit minor edits

--- app/components/AllNotes.tsx
+++ app/components/AllNotes.tsx
@@ -55,7 +55,7 @@ export default function Notes() {
                 height="16"
                 fill="currentColor"
                 viewBox="0 0 16 16"
-                style={{ marginLeft: '5px' }}
+                style={{ marginLeft: '7px' }}
               >
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
               </svg>
